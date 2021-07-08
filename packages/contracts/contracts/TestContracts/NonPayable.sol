acb8e6baf13e7f0996e8d07b66d2f75cedbd1a6e
Yash Agrawal <yashagrawal18@outlook.com>
2021-07-08 15:11:21 +0530

    Revert NonPayable.sol file

--- packages/contracts/contracts/TestContracts/NonPayable.sol
+++ packages/contracts/contracts/TestContracts/NonPayable.sol
@@ -13,7 +13,7 @@ contract NonPayable {
     }
 
     function forward(address _dest, bytes calldata _data) external payable {
-        (bool success, bytes memory returnData) = _dest.call(_data);
+        (bool success, bytes memory returnData) = _dest.call{ value: msg.value }(_data);
         //console.logBytes(returnData);
         require(success, string(returnData));
     }
