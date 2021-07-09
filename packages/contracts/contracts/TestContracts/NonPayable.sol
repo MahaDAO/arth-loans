68c130ed264b57482b2ccda28e337fac38897431
Yash Agrawal <yashagrawal18@outlook.com>
2021-07-09 13:44:45 +0530

    Minor fix

--- packages/contracts/contracts/TestContracts/NonPayable.sol
+++ packages/contracts/contracts/TestContracts/NonPayable.sol
@@ -3,17 +3,25 @@
 pragma solidity 0.6.11;
 
 //import "../Dependencies/console.sol";
-
+import "../Dependencies/IERC20.sol";
 
 contract NonPayable {
     bool isPayable;
+
+    IERC20 weth;
+
+    constructor(IERC20 token) public {
+        weth = token;
+    }
 
     function setPayable(bool _isPayable) external {
         isPayable = _isPayable;
     }
 
-    function forward(address _dest, bytes calldata _data) external payable {
-        (bool success, bytes memory returnData) = _dest.call{ value: msg.value }(_data);
+    function forward(address _dest, bytes calldata _data, uint256 _amount) external payable {
+        weth.transferFrom(msg.sender, address(this), _amount);
+        weth.approve(_dest, _amount);
+        (bool success, bytes memory returnData) = _dest.call(_data);
         //console.logBytes(returnData);
         require(success, string(returnData));
     }
