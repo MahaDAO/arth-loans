cfb1687579acc4a2ef5d24564f55ae01afe94af0
Yash Agrawal <yashagrawal18@outlook.com>
2021-07-14 01:04:08 +0530

    Add some contracts from ARTHV2

---
+++ packages/contracts/contracts/Dependencies/ARTHBasicERC20.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.11;
+
+import "./SafeMath.sol";
+import "./Address.sol";
+import {ERC20} from "./ERC20.sol";
+
+/**
+ * @dev Missing feature in the Liquity Base ERC20 when compare 
+ * to ARTH basic ERC20.
+ */
+contract ARTHBasicERC20 is ERC20 {
+    using SafeMath for uint256;
+
+    /**
+     * @dev Sets the values for {name} and {symbol}, initializes {decimals} with
+     * a default value of 18.
+     *
+     * To select a different value for {decimals}, use {_setupDecimals}.
+     *
+     * All three of these values are immutable: they can only be set once during
+     * construction.
+     */
+    constructor (string memory name_, string memory symbol_) public ERC20(name_, symbol_) {}
+
+    /**
+     * @dev Destroys `amount` tokens from the caller.
+     *
+     * See {ERC20-_burn}.
+     */
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
+     * allowance.
+     *
+     * See {ERC20-_burn} and {ERC20-allowance}.
+     *
+     * Requirements:
+     *
+     * - the caller must have allowance for `accounts`'s tokens of at least
+     * `amount`.
+     */
+    function burnFrom(address account, uint256 amount) public virtual {
+        uint256 decreasedAllowance =
+            allowance(account, _msgSender()).sub(
+                amount,
+                'ERC20: burn amount exceeds allowance'
+            );
+
+        _approve(account, _msgSender(), decreasedAllowance);
+        _burn(account, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`.`amount` is then deducted
+     * from the caller's allowance.
+     *
+     * See {_burn} and {_approve}.
+     */
+    function _burnFrom(address account, uint256 amount) internal virtual {
+        _burn(account, amount);
+        _approve(
+            account,
+            _msgSender(),
+            _allowances[account][_msgSender()].sub(
+                amount,
+                'ERC20: burn amount exceeds allowance'
+            )
+        );
+    }
+    
+}
--- packages/contracts/contracts/Dependencies/Ownable.sol
+++ packages/contracts/contracts/Dependencies/Ownable.sol
@@ -2,6 +2,8 @@
 
 pragma solidity 0.6.11;
 
+import "./Context.sol";
+// MOdificaitons taken from: `https://github.com/FraxFinance/frax-solidity/blob/master/src/hardhat/contracts/Common/Ownable.sol`
 /**
  * Based on OpenZeppelin's Ownable contract:
  * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
@@ -14,7 +16,7 @@ pragma solidity 0.6.11;
  * `onlyOwner`, which can be applied to your functions to restrict their use to
  * the owner.
  */
-contract Ownable {
+contract Ownable is Context {
     address private _owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
@@ -23,8 +25,9 @@ contract Ownable {
      * @dev Initializes the contract setting the deployer as the initial owner.
      */
     constructor () internal {
-        _owner = msg.sender;
-        emit OwnershipTransferred(address(0), msg.sender);
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
     }
 
     /**
@@ -46,7 +49,7 @@ contract Ownable {
      * @dev Returns true if the caller is the current owner.
      */
     function isOwner() public view returns (bool) {
-        return msg.sender == _owner;
+        return _msgSender() == _owner;
     }
 
     /**
@@ -62,5 +65,15 @@ contract Ownable {
     function _renounceOwnership() internal {
         emit OwnershipTransferred(_owner, address(0));
         _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
     }
 }
