e408c573ffb2ce29e08164a101403c64dcb62e3d
Yash Agrawal <yashagrawal18@outlook.com>
2021-07-22 22:35:00 +0530

    Fix Ownable contract issues

--- packages/contracts/contracts/GMUOracle.sol
+++ packages/contracts/contracts/GMUOracle.sol
@@ -3,9 +3,9 @@
 pragma solidity 0.6.11;
 
 import "./Interfaces/IOracle.sol";
-import "./Dependencies/Ownable.sol";
+import "./Dependencies/ARTHOwnable.sol";
 
-contract GMUOracle is Ownable, IOracle {
+contract GMUOracle is ARTHOwnable, IOracle {
     uint256 public price = 1e6;
     string constant public NAME = "GMU/USD Oracle";
 
