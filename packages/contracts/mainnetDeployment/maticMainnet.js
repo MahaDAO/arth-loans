a7690d748c04c7d4de3e826633cdc863b4ba2399
Yash Agrawal <yashagrawal18@outlook.com>
2021-08-15 02:08:13 +0530

    Update matic mainnet deployment scripts

---
+++ packages/contracts/mainnetDeployment/maticMainnet.js
@@ -0,0 +1,35 @@
+const configParams = require("./params/matic.js")
+const MainnetDeploymentHelper = require("../utils/maticMainnetDeploymentHelper.js")
+
+async function mainnetDeploy() {
+    const date = new Date()
+    const gasPrice = configParams.GAS_PRICE
+  
+    console.log('Deployment start time', date.toUTCString())
+    console.log('Gas price', gasPrice.toString());
+  
+    const deployerWallet = (await ethers.getSigners())[0]
+    
+    const mdh = new MainnetDeploymentHelper(configParams, deployerWallet)
+    await mdh.loadAllContractFactories()
+    const deploymentState = mdh.loadPreviousDeployment()
+  
+    console.log(`Deployer address: ${deployerWallet.address}`)
+    assert.equal(deployerWallet.address, configParams.DEPLOYER_ADDRS.DEPLOYER)
+    let deployerETHBalance = await ethers.provider.getBalance(deployerWallet.address)
+    console.log(`Deployer ETH balance before: ${deployerETHBalance}`)
+  
+    // Deploy core logic contracts.
+    await mdh.deployTestnet(deploymentState)
+  
+    deployerETHBalance = await ethers.provider.getBalance(deployerWallet.address)
+    console.log(`Deployer's ETH balance after deployments: ${deployerETHBalance}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
+
