f11837546164e34eafa8a250871d624fb70ad783
Yash Agrawal <yashagrawal2829@gmail.com>
2022-02-08 18:27:00 +0530

    feat: tokenize the staking pools

--- packages/contracts/contracts/Interfaces/IStakingToken.sol
+++ packages/contracts/contracts/Interfaces/IStakingToken.sol
@@ -5,11 +5,6 @@ pragma solidity 0.6.11;
 import { IERC20 } from "../Dependencies/IERC20.sol";
 
 interface IStakingToken is IERC20 {
-    function mintOnStake(address recipient_, uint256 amount_)
-        external
-        returns (bool);
-
-    function burnOnWithdraw(address recipient_, uint256 amount_)
-        external 
-        returns (bool);
+    function deposit(uint256 _amount) external;
+    function withdraw(uint256 _amount) external;
 }
--- packages/contracts/contracts/StakingToken.sol
+++ packages/contracts/contracts/StakingToken.sol
@@ -2,47 +2,72 @@
 
 pragma solidity 0.6.11;
 
-import { ERC20 } from "./Dependencies/ERC20.sol";
+import { ERC20, IERC20 } from "./Dependencies/ERC20.sol";
+import { IMasterChef } from "./Dependencies/IMasterChef.sol";
 import { IStakingToken } from "./Interfaces/IStakingToken.sol";
-import { TransferableOwnable } from "./Dependencies/TransferableOwnable.sol";
 
-contract StakingToken is ERC20, TransferableOwnable, IStakingToken {
-    address public underlyingStakingToken;
+contract StakingToken is ERC20, IStakingToken {
+    uint256 public poolId;
+    string public CONTRACT_NAME;
+
+    IERC20 public token;
+    IERC20 public reward;
+    IMasterChef public pool;
 
     constructor(
+        uint256 poolId_,
         string memory name_, 
-        string memory symbol_, 
-        address _underlyingStakingToken
-    ) 
+        string memory symbol_,
+        string memory CONTRACT_NAME_,
+        IERC20 token_,
+        IERC20 reward_,
+        IMasterChef pool_
+    )
         ERC20(name_, symbol_) 
         public 
     {
-        underlyingStakingToken = _underlyingStakingToken;
+        poolId = poolId_;
+        CONTRACT_NAME = CONTRACT_NAME_;
+
+        pool = pool_;
+        token = token_;
+        reward = reward_;
+        
     }
 
-    function mintOnStake(address recipient_, uint256 amount_)
-        public
-        onlyOwner
-        override
-        returns (bool)
-    {
-        uint256 balanceBefore = balanceOf(recipient_);
-        _mint(recipient_, amount_);
-        uint256 balanceAfter = balanceOf(recipient_);
+    function deposit(uint256 _amount) external override {
+        require(_amount > 0, "Staking: amount is 0");
 
-        return balanceAfter > balanceBefore;
+        token.transferFrom(msg.sender, address(this), _amount);
+        token.approve(address(pool), _amount);
+
+        uint256 balanceBefore = reward.balanceOf(address(this));
+        if (poolId == 0) {
+            pool.enterStaking(_amount);
+        } else {
+            pool.deposit(poolId, _amount);
+        }
+        uint256 balanceAfter = reward.balanceOf(address(this));
+        uint256 rewardAmount = balanceAfter.sub(balanceBefore);
+
+        if (rewardAmount > 0) reward.transfer(msg.sender, rewardAmount);
+        _mint(msg.sender, _amount);
     }
 
-    function burnOnWithdraw(address recipient_, uint256 amount_)
-        public 
-        onlyOwner
-        override 
-        returns (bool) 
-    {
-        uint256 balanceBefore = balanceOf(recipient_);
-        _burn(recipient_, amount_);
-        uint256 balanceAfter = balanceOf(recipient_);
+    function withdraw(uint256 _amount) external override {
+        require(_amount <= balanceOf(msg.sender), "Staking: amount > balance");
 
-        return balanceAfter < balanceBefore;
+        uint256 balanceBefore = reward.balanceOf(address(this));
+        if (poolId == 0) {
+            pool.leaveStaking(_amount);
+        } else {
+            pool.withdraw(poolId, _amount);
+        }
+        uint256 balanceAfter = reward.balanceOf(address(this));
+        uint256 rewardAmount = balanceAfter.sub(balanceBefore);
+
+        token.transfer(msg.sender, _amount);
+        if (rewardAmount > 0) reward.transfer(msg.sender, rewardAmount);
+        _burn(msg.sender, _amount);
     }
 }
