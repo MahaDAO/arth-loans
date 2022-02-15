99645a2d3ccb67aae22fa25cece9607f668cf0e7
Yash Agrawal <yashagrawal2829@gmail.com>
2022-02-15 15:00:55 +0530

    feat: add basic leverage using flashloan

---
+++ packages/contracts/contracts/Leverage/FlashLoanLeverage.sol
@@ -0,0 +1,154 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.11;
+
+import "../Dependencies/CheckContract.sol";
+import "../Interfaces/IBorrowerOperations.sol";
+import "../Dependencies/IUniswapV2Router02.sol";
+import "../Dependencies/IUniswapV2Pair.sol";
+import "../Dependencies/IERC20.sol";
+import "../Dependencies/SafeMath.sol";
+import "../Interfaces/IFlashLoan.sol";
+
+contract FlashLoanLeverage is CheckContract {
+    using SafeMath for uint256;
+
+    address immutable arth;
+    address immutable pair;
+    address immutable token0;
+    address immutable token1;
+    address[] public arthToToken0Path;
+    address[] public arthToToken1Path;
+
+    IFlashLoan public flashLoan;
+    IUniswapV2Router02 public uniswapRouter;
+    IBorrowerOperations public borrowerOperations;
+
+    constructor(
+        address _pair,
+        address _arth,
+        address[] memory _arthToToken0Path,
+        address[] memory _arthToToken1Path,
+        IFlashLoan _flashLoan,
+        IUniswapV2Router02 _uniswapRouter,
+        IBorrowerOperations _borrowerOperations
+    ) public {
+        CheckContract(_arth);
+        CheckContract(_pair);
+        checkContract(address(_uniswapRouter));
+        CheckContract(address(_flashLoan));
+        checkContract(address(_borrowerOperations));
+
+        arth = _arth;
+        pair = _pair;
+        token0 = IUniswapV2Pair(_pair).token0();
+        token1 = IUniswapV2Pair(_pair).token1();
+        arthToToken0Path = _arthToToken0Path;
+        arthToToken1Path = _arthToToken1Path;
+        flashLoan = IFlashLoan(_flashLoan);
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
+        borrowerOperations = _borrowerOperations;
+    }
+
+    function leverage(
+        bytes calldata params,
+        uint256 flashLoanAmount
+    ) external {
+        flashLoan.flashLoan(address(this), flashLoanAmount, params);
+    }
+
+    function executeOperation(
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external returns(bool) {
+        require(msg.sender == address(flashLoan), "Untrusted lender");
+        require(initiator == address(this), "Untrusted initiator");
+    
+        uint256 paybackAmount = amount.add(premium);
+
+        (
+            uint256 _maxFee,
+            uint256 _LUSDAmount,
+            uint256 _ETHAmount,
+            address _upperHint,
+            address _lowerHint,
+            address _frontEndTag
+        ) = abi.decode(
+            params, 
+            (uint256,uint256,uint256,address,address,address)
+        );
+
+        // 1. Swap and add liquidity for the borrowed ARTH.
+        uint256 arthToSwapForToken0 = amount.div(2);
+        uint256 arthToSwapForToken1 = amount.sub(arthToSwapForToken0);
+        uint256 token0Out = _swapARTHForToken(arthToToken0Path, arthToSwapForToken0);
+        uint256 token1Out = _swapARTHForToken(arthToToken1Path, arthToSwapForToken1);
+        uint256 liquidityOut = _addLiquidity(token0Out, token1Out);
+
+        // 2. Take the LP token and approve it to be taken in by borrower operations.
+        IERC20(pair).transferFrom(msg.sender, address(this), _ETHAmount);
+        IERC20(pair).approve(address(borrowerOperations), _ETHAmount.add(liquidityOut));
+
+        // 3. Borrow ARTH.
+        borrowerOperations.openTrove(
+            _maxFee, 
+            _LUSDAmount, 
+            _ETHAmount, 
+            _upperHint, 
+            _lowerHint, 
+            _frontEndTag
+        );
+        require(
+            IERC20(arth).balanceOf(address(this)) >= paybackAmount,
+            "Wrong payback amount"
+        );
+        IERC20(arth).approve(address(flashLoan), paybackAmount);
+
+        return true;
+    }
+
+    function _swapARTHForToken(
+        address[] memory path,
+        uint256 _arthAmount
+    ) internal returns (uint256) {
+        IERC20(arth).approve(address(uniswapRouter), _arthAmount);
+
+        uint256[] memory expectedAmountsOut = uniswapRouter.getAmountsOut(
+            _arthAmount,
+            path
+        );
+
+        uint256[] memory amountsOut = uniswapRouter.swapExactTokensForTokens(
+            _arthAmount,
+            expectedAmountsOut[expectedAmountsOut.length - 1],
+            path,
+            address(this),
+            block.timestamp
+        );
+
+        return amountsOut[amountsOut.length - 1];
+    }
+
+    function _addLiquidity(
+        uint256 _token0Amount,
+        uint256 _token1Amount
+    ) internal returns (uint256) {
+        IERC20(token0).approve(address(uniswapRouter), _token0Amount);
+        IERC20(token1).approve(address(uniswapRouter), _token1Amount);
+
+        (,, uint256 liquidity) = uniswapRouter.addLiquidity(
+            token0,
+            token1,
+            _token0Amount,
+            _token1Amount,
+            _token0Amount,
+            _token1Amount,
+            address(this),
+            block.timestamp
+        );
+
+        return liquidity;
+    }
+}
